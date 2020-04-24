72150aa2238867451a0b5af76d43c422c0bb11c7
Mariana <mxpixel@yahoo.com.br>
2020-04-24 19:13:12 -0300

    Importando API

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import streamlit as st
+import requests as rq
+import json as js
 
 def main():
     st.title('Hello Wolrd')
 
+    request = rq.get('https://api.thecatapi.com/v1/breeds')
+
+    request.headers['x-api-key'] = 'a4824c2c-86fb-4ec8-8627-b5779c3bc0be'
+
+    response_api = js.loads(request.text)
+
+    st.markdown(response_api == request.json())
+
+    response_api[:5]
+
+
 if __name__ == '__main__':
     main()
 
-#https://docs.thecatapi.com/api-reference/breeds/breeds-list
+
